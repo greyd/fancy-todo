ba62773f632a7b3270a623e8bdb99aaf4ab9e76a
Valerii Pavlov <pavlov.valerii@gmail.com>
2016-08-23 17:08:06 +0300

    Initial commit

---
+++ src/shared/components/About/About.js
@@ -0,0 +1,14 @@
+/* @flow */
+
+import React from 'react';
+import Helmet from 'react-helmet';
+
+function About() {
+  return (
+    <div>
+      <Helmet title="About" />
+    </div>
+  );
+}
+
+export default About;
